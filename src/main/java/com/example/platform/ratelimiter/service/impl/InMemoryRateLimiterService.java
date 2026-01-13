08d91ff9be14eeabb13c58a47788a7421d1a596c
Gaurav Jain <gauravjain200024@gmail.com>
2026-01-13 18:44:57 +0530

    Updated Service Code

--- src/main/java/com/example/platform/ratelimiter/service/impl/InMemoryRateLimiterService.java
+++ src/main/java/com/example/platform/ratelimiter/service/impl/InMemoryRateLimiterService.java
@@ -2,38 +2,47 @@ package com.example.platform.ratelimiter.service.impl;
 
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.atomic.AtomicInteger;
-
 import org.springframework.stereotype.Service;
 
+import com.example.platform.ratelimiter.config.RateLimitProperties;
+import com.example.platform.ratelimiter.model.RateLimitBucket;
+import com.example.platform.ratelimiter.model.RateLimitRule;
 import com.example.platform.ratelimiter.service.RateLimiterService;
 
 @Service
 public class InMemoryRateLimiterService implements RateLimiterService {
 
-    private static final int LIMIT = 5;
-    private static final long WINDOW_MS = 60_000;
+    private final Map<String, RateLimitBucket> buckets = new ConcurrentHashMap<>();
+    private final Map<String, RateLimitRule> ruleMap = new ConcurrentHashMap<>();
 
-    private final Map<String, AtomicInteger> counters = new ConcurrentHashMap<>();
-    private final Map<String, Long> windowStart = new ConcurrentHashMap<>();
+    public InMemoryRateLimiterService(RateLimitProperties properties){
+        for(RateLimitRule rule: properties.getRules()){
+            String ruleKey = buildRuleKey(rule.getApi(), rule.getHttpMethod());
+            ruleMap.put(ruleKey, rule);
+        }
+    }
 
     @Override
     public boolean isRequestAllowed(String clientId, String api, String httpMethod) {
-        long now = System.currentTimeMillis();
-        String key = buildKey(clientId, api, httpMethod);
+        String ruleKey = buildRuleKey(api, httpMethod);
+        RateLimitRule rule = ruleMap.get(ruleKey);
 
-        windowStart.putIfAbsent(key, now);
-        counters.putIfAbsent(key, new AtomicInteger(0));
-
-        if (now - windowStart.get(key) > WINDOW_MS) {
-            windowStart.put(key, now);
-            counters.get(key).set(0);
+        if(null == rule){
+            return true;
         }
 
-        return counters.get(key).incrementAndGet() <= LIMIT;
+        String bucketKey = buildBucketKey(clientId, api, httpMethod);
+
+        RateLimitBucket bucket = buckets.computeIfAbsent(bucketKey, k->new RateLimitBucket(rule.getLimit(), rule.getWindowInSeconds()));
+
+        return bucket.allowRequest();
     }
 
-    private String buildKey(String clientId, String api, String httpMethod) {
+    private String buildBucketKey(String clientId, String api, String httpMethod) {
         return clientId + "|" + api + "|" + httpMethod;
+    }
+
+    private String buildRuleKey(String api, String method){
+        return api + "|" + method;
     }
 }
